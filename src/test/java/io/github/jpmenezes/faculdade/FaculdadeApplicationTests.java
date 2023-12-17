5f18d5a29ef3439980d54496a7cd0407aabb8fd9
Menezes <jpgam321@gmail.com>
2023-12-17 12:40:25 -0300

    Test: SpringTest - Classe não utilizada

---
+++ src/test/java/io/github/jpmenezes/faculdade/FaculdadeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jpmenezes.faculdade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaculdadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
